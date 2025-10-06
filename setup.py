e9db71ebf29bc9f0444f0518b43e9fcafa7a040f
Git179-user <kyalocharles179@gmail.com>
2025-10-06 17:18:24 +0300

    Added package config

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mypackage',
-    version='0.1',
+    name="mypackage",
+    version="0.1.0",
     packages=find_packages(exclude=['tests*']),
-    license='MIT',
-    description='EDSA example python package',
-    long_description=open('README.md').read(),
-    install_requires=['numpy'],
-    url='https://github.com/Git179-user/mypackage',
-    author='Charles Kyalo',
-    author_email='kyalocharles179@gmail.com'
+    description= 'EDSA example python package',
+    author="Charles Kyalo",
+    author_email="kyalocharles179@gmail.com",
+    url="https://github.com/Git179-user/mypackage",
+    install_requires=[
+        # List your dependencies here, e.g.:
+        "numpy>=1.20.0",
+        "pandas>=1.3.0",
+    ],
+    python_requires=">=3.6",
 )
+
+
+
